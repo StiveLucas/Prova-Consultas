261d3d8ba3260b4fae1a0e336d7afd5872f633c6
StiveLucas <161142347+StiveLucas@users.noreply.github.com>
2025-05-13 00:44:55 -0300

    Delete NetBeansProjects/GerenciamentoConsultas directory

--- NetBeansProjects/GerenciamentoConsultas/src/main/java/com/mycompany/gerenciamentoconsultas/controle/ConsultaControle.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.gerenciamentoconsultas.controle;
-
-import com.mycompany.gerenciamentoconsultas.DAO.ConsultaDAO;
-
-public class ConsultaControle {
-    private ConsultaDAO dao = new ConsultaDAO();
-    
-}
